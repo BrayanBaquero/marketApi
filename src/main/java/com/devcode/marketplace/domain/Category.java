7bc411bbbe5ff7a2d9cc2a85a391b058a8976715
Brayan Baquero <brayan.baquero.cz@gmail.com>
2022-04-18 17:43:03 -0500

    add mappers

---
+++ src/main/java/com/devcode/marketplace/domain/Category.java
@@ -0,0 +1,12 @@
+package com.devcode.marketplace.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Category {
+    private int categoryId;
+    private String category;
+    private boolean active;
+}
